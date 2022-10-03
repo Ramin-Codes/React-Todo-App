2b2796b04ffed01ebbcdf88b8f66a8a6dcbf225a
Ramin-Codes <mr.yousefpour94@gmail.com>
2022-10-03 11:34:10 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react'
+
+
 
 function App() {
+  const [todoText, setTodoText] = useState('')
+  const [todos, setTodos] = useState([])
+  
+
+  /*----------TodoTextFunc----------*/
+  const setTodoTextFunc = useCallback((e) => {
+    setTodoText(e.target.value)
+  }, [])
+
+
+  /*----------SubmitForm----------*/
+  const submitForm = useCallback(e => {
+    e.preventDefault()
+    if (!todoText.trim()) return
+
+    setTodos([
+      {
+        id: todos.length ? todos[0].id + 1 : 1,
+        text: todoText ,
+        done: false
+      },
+      ...todos, 
+    ])
+    setTodoText('')
+
+  }, [todoText, todos])
+
+
+
+  /*----------HandleCheckbox----------*/
+  const handleCheckbox = useCallback((todo, index) => () => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1, {
+      ...todo,
+      done: !todo.done
+    })
+    setTodos(newTodos)
+  }, [todos])
+
+
+
+  /*----------RemoveTodo----------*/
+  const removeTodo = useCallback((todo) => () => {
+    setTodos(todos.filter(todoItem => todoItem !== todo))
+  }, [todos])
+
+
+
+  /*----------MarkAllDone----------*/
+  const markAllDone = useCallback(() => {
+    setTodos(todos.map(todo => ({
+      ...todo,
+      done: true
+    })))
+  }, [todos])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+
+      {/*----------FormInput----------*/}
+      <form onSubmit={submitForm}>
+        <input type="text" value={todoText} onChange={setTodoTextFunc} />
+        <button>Add Todo</button>
+      </form>
+
+      {/*----------Todos----------*/}
+      <button onClick={markAllDone}>Mark All Done</button>
+      <ul>
+        {
+          todos?.map((todo, index) => {
+            return (
+              <li key={todo.id}>
+
+                <p style={{ textDecoration: todo.done && 'line-through'}}>{todo.text}</p>
+
+                <input 
+                  type="checkbox" 
+                  checked={todo.done} 
+                  onChange={handleCheckbox(todo, index)}
+                />
+                
+                <button onClick={removeTodo(todo)}>remove</button>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
---
+++ src/features.js
@@ -0,0 +1,24 @@
+// import styled, {css, keyframes,  createGlobalStyle, ThemeProvider } from 'styled-components'
+
+
+// import { Routes, Route, useParams, Outlet, useLocation, NavLink, useSearchParams, useOutletContext, useMatch } from 'react-router-dom'
+
+
+// import { animateScroll as scroll } from 'react-scroll'
+
+
+// import { 
+//   motion,
+//   useCycle,
+//   useAnimation, 
+//   AnimatePresence,
+//   useElementScroll,
+//   useViewportScroll,
+//   useMotionValue,
+//   useTransform,
+//   Reorder,
+//   useSpring, 
+//   useTime,
+//   useMotionTemplate,
+//   useAnimationFrame,
+//   } from 'framer-motion'
